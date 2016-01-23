22a7f3d13b8addaf52f8609c364938788750f63d
Cube <tommi__@live.com>
2016-01-23 18:41:29 +0200

    Remove obsolete queue from ClientEvenReceiver

--- core/src/main/java/com/scapelog/client/event/ClientEventReceiver.java
+++ core/src/main/java/com/scapelog/client/event/ClientEventReceiver.java
@@ -1,48 +1,36 @@
 package com.scapelog.client.event;
 
+import com.scapelog.agent.util.InjectionUtils;
 import com.scapelog.api.ClientFeature;
 import com.scapelog.api.ClientFeatureStatus;
 import com.scapelog.api.event.Event;
+import com.scapelog.client.ScapeLog;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-
-public final class ClientEventReceiver implements Runnable {
-	private final static BlockingQueue<String> eventQueue = new LinkedBlockingQueue<>();
+public final class ClientEventReceiver {
 
 	public static void receive(String eventString) {
-		try {
-			eventQueue.add(eventString);
-		} catch (Exception e) {
-			/**/
-		}
-	}
-
-	@Override
-	public void run() {
-		for (;;) {
+		ScapeLog.getExecutor().submit(() -> {
 			try {
-				String eventString = eventQueue.take();
-				String[] parts = eventString.split("_");
+				String[] parts = eventString.split(InjectionUtils.SEPARATOR);
 				if (parts.length > 0) {
 					String identifier = parts[0];
 					ClientFeature feature = ClientFeature.getFeature(identifier);
 					if (feature == null) {
-						continue;
+						return;
 					}
 					if (feature.getStatus() == ClientFeatureStatus.UNVERIFIED) {
 						feature.setStatus(ClientFeatureStatus.ENABLED);
 					}
 					Event event = ClientFeature.parseEvent(identifier, parts);
 					if (event == null) {
-						continue;
+						return;
 					}
 					EventDispatcher.fireEvent(event);
 				}
-			} catch (InterruptedException e) {
+			} catch (Exception e) {
 				/**/
 			}
-		}
+		});
 	}
 
 }
